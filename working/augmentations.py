472ac95b27788ac4028d31e0412136316260c0b3
sidhanthholalkere <sidhanth.holalkere@gmail.com>
2019-11-10 14:58:50 -0500

    fix infer

--- working/augmentations.py
+++ working/augmentations.py
@@ -4,7 +4,7 @@ def training1(h=320, w=640):
     train_transform = [
 
         albu.HorizontalFlip(p=0.5),
-        albu.VerticalFlip(p=0.5))
+        albu.VerticalFlip(p=0.5),
         albu.ShiftScaleRotate(scale_limit=0.5, rotate_limit=0, shift_limit=0.1, p=0.5, border_mode=0),
         albu.GridDistortion(p=0.5),
         albu.OpticalDistortion(p=0.5, distort_limit=2, shift_limit=0.5),
